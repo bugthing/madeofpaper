54d709b2b9b6075851aefce679c08a330de3ad9a
Benjamin Martin <ben@madeofpaper.co.uk>
2019-03-16 22:26:57 +0000

    added uuid

--- src/js/chart-builder.js
+++ src/js/chart-builder.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import uuidv1 from 'uuid/v1'
 import ChartNode from './chart-node';
 import ChartLine from './chart-line';
 import { ChartContext } from './chart-context';
@@ -11,7 +12,7 @@ class ChartBuilder extends React.Component {
   }
 
   handleNodeClick(e) {
-    console.log('node clicked:');
+    console.log('node clicked:', this);
     e.stopPropagation();
   }
 
@@ -19,6 +20,7 @@ class ChartBuilder extends React.Component {
     let nodes = [
       ...this.state.nodes,
       {
+        id: uuidv1(),
         x: e.nativeEvent.offsetX,
         y: e.nativeEvent.offsetY
       }
